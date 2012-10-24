621cd674af9d389ea5e2a2bd33107aa5533b518e
dsherry <sharshofski@gmail.com>
2012-10-23 20:35:59 -0400

    Reorganized svm and svm_test testing structure. svm doesn't work yet, but I know why: 1) not multiplying "b" (also called "w0") by y_i in rows of matrix A 2) need to add constraints specifying eps_i >= 0. This means adding n more rows to A and g
    Fixing both these problems should produce results.

--- svm.py
+++ svm.py
@@ -7,20 +7,12 @@ import scipy.optimize
 import cvxopt, cvxopt.solvers
 
 from logreg import make_phi
+numpy.set_printoptions(threshold=numpy.nan)
 
 ## primal:
 ## input x, y, C
-## TODO get data -- convert to classes
-name='ls'
-train = numpy.loadtxt('data/data_'+name+'_train.csv')
-# use deep copy here to make cvxopt happy
-x = train[:, 0:2].copy()
-y = train[:, 2:3].copy()
-C = 0
-def primal(x,y,C):
-    ## make phi
-    phi = make_phi(x,2)
 
+def primal(phi,y,C):
     ## primal quad prog.
     ## let n be the number of data points, and let m be the number of features
     n,m = phi.shape
@@ -29,49 +21,84 @@ def primal(x,y,C):
     Q = numpy.zeros((m+n+1,m+n+1))
     for i in range(m):
         Q[i,i] = 1
+    #print Q[0:7,0:7]
     c = numpy.vstack([numpy.zeros((m+1,1)), C*numpy.ones((n,1))])
+    #print c[0:10]
     A = numpy.zeros((n, m+1+n))
     A[:,0:m] = y*phi
-    A[:,n] = 1
+    A[:,m] = 1
     A[:,m+1:m+1+n]  = numpy.eye(n)
+    #print A[1,:]
     A = -A
     g = -numpy.ones((n,1))
-    print Q.shape, c.shape, A.shape, g.shape
     E = d = 0
     ## convert to array
+    print numpy.linalg.matrix_rank(Q), numpy.linalg.matrix_rank(c), numpy.linalg.matrix_rank(A), numpy.linalg.matrix_rank(g)
+    #comp = numpy.vstack([Q,A])
+    #print comp.shape, numpy.linalg.matrix_rank(comp)
+    print "n,m=",n,m
+    print "Q:",Q.shape
+    print Q
+    print "c:",c.shape
+    print c
+    print "A:",A.shape
+    print A
+    print "g:",g.shape
+    print g
     Q = cvxopt.matrix(Q,Q.shape,'d')
     c = cvxopt.matrix(c,c.shape,'d')
     A = cvxopt.matrix(A,A.shape,'d')
     g = cvxopt.matrix(g,g.shape,'d')
+    print "n,m=",n,m
+    print "Q:",Q.size
+    print Q
+    print "c:",c.size
+    print c
+    print "A:",A.size
+    print A
+    print "g:",g.size
+    print g
     ## set up cvxopt
+    #sol = cvxopt.solvers.qp(Q, c, A, g)
     sol = cvxopt.solvers.qp(Q, c, A, g)
     return sol
 
 ## the dual takes a kernel as well
-def dual(x,y,C,k):
+def dual(phi,y,C,K):
     ## TODO finish this
-    ## make phi
-    phi = make_phi(x)
-
-    ## primal quad prog.
-    ## let n be the number of data points, and let m be the number of features
-    n,m = phi.shape
-    ## z is [w, b, eps].T
-    z = numpy.array([0]*m + [0] + [0]*n)
-    Q = numpy.zeros((m+n,m+n))
-    for i in range(m):
-        Q[i,i] = 1
-    c = numpy.vstack([numpy.zeros((m+1,1)), C*numpy.ones((n,1))])
-    A = numpy.zeros((n, m+1+n))
-    A[:,0:m] = y*phi
-    A[:,n] = 1
-    A[:,m+1:m+1+n]  = numpy.eye(n)
-    A = -A
-    g = -numpy.ones((n,1))
-    E = d = 0
-    ## set up cvxopt
-    sol = cvxopt.solvers.qp(Q, c, A, g)
-    return sol
+    pass
 
 if __name__=='__main__':
-    print primal(x,y,1)
+    # ## test out training
+    # name='ls'
+    # train = numpy.loadtxt('data/data_'+name+'_train.csv')
+    # x = train[:, 0:2].copy()
+    # M = 2
+    # phi = make_phi(x,M)
+    # y = train[:, 2:3].copy()
+    # C = .01
+
+    # p=primal(phi,y,C)
+    # print p
+
+    ## a very simple training test
+    numpy.set_printoptions(threshold=numpy.nan)
+    M = 2
+    xDummy = numpy.array([[1,0],[-1,0]])
+    assert xDummy.shape == (2,2)
+    phiDummy = make_phi(xDummy,M)
+    assert phiDummy.shape == (2,6)
+    print phiDummy
+    #n,m = phiDummy.shape
+    n,m = xDummy.shape
+    yDummy = numpy.array([[1], [-1]])
+    assert yDummy.shape == (2,1)
+
+    # Carry out training, primal and/or dual
+    C = 0.25
+    #C = 0
+    #p = primal(phiDummy,yDummy,C)
+    p2 = primal(xDummy,yDummy,C)
+    w = numpy.array(p2['x'][:m])
+#    assert w.shape == (6,1)
+    b = p2['x'][m]
--- svm_test.py
+++ svm_test.py
@@ -1,29 +1,76 @@
 from numpy import *
 from plotBoundary import *
 # import your SVM training code
+import svm
+import numpy
+from logreg import make_phi
 
-# parameters
-name = 'ls'
-print '======Training======'
-# load data from csv files
-train = loadtxt('data/data_'+name+'_train.csv')
-# use deep copy here to make cvxopt happy
-X = train[:, 0:2].copy()
-Y = train[:, 2:3].copy()
-
-# Carry out training, primal and/or dual
+M=2
 
 # Define the predictSVM(x) function, which uses trained parameters
+def makePredictor(w,b,M):
+    def predict(x):
+        #print x.shape, w.shape
+        ## transform into feature space
+        phi = make_phi(x,M)
+        #print phi.shape, w.shape
+        val = phi.dot(w) + b
+        return 1 if val > 0 else -1
+    return predict
 
-# plot training results
-plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Train')
+def train():
+    # parameters
+    name = 'ls'
+    print '======Training======'
+    # load data from csv files
+    train = loadtxt('data/data_'+name+'_train.csv')
+    # use deep copy here to make cvxopt happy
+    X = train[:, 0:2].copy()
+    M=2
+    phi = make_phi(X,M)
+    n,m = phi.shape
+    Y = train[:, 2:3].copy()
 
+    # Carry out training, primal and/or dual
+    p = svm.primal(phi,Y,0.01)
+    w = numpy.array(p['x'][:m])
+    b = p['x'][m]
 
-print '======Validation======'
-# load data from csv files
-validate = loadtxt('data/data_'+name+'_validate.csv')
-X = validate[:, 0:2]
-Y = validate[:, 2:3]
-# plot validation results
-plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Validate')
+    predictSVM = makePredictor(w,b,M)
 
+    # plot training results
+    plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Train')
+
+def validate():
+    print '======Validation======'
+    # load data from csv files
+    validate = loadtxt('data/data_'+name+'_validate.csv')
+    X = validate[:, 0:2]
+    Y = validate[:, 2:3]
+    # plot validation results
+    plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Validate')
+
+def dummy():
+    ## a very simple test
+    xDummy = numpy.array([[0,0],[1,1],[1,2]])
+    assert xDummy.shape == (3,2)
+    phiDummy = make_phi(xDummy,M)
+    assert phiDummy.shape == (3,6)
+    n,m = phiDummy.shape
+    yDummy = numpy.array([[-1], [1], [1]])
+    assert yDummy.shape == (3,1)
+
+    # Carry out training, primal and/or dual
+    C = 10e10
+    p = svm.primal(phiDummy,yDummy,C)
+    w = numpy.array(p['x'][:m])
+    assert w.shape == (1,6)
+    b = p['x'][m]
+    assert isinstance(b, (int,float))
+    dummyPredictor = makePredictor(w,b,M)
+
+    plotDecisionBoundary(xDummy, yDummy, dummyPredictor, [-1, 0, 1], title = 'SVM Validate')
+
+if __name__=='__main__':
+    dummy()
+    #pl.show()
